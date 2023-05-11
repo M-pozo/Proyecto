057532f981d800cf9ebe7e3d19a4f6d10bc232cb
M-pozo <m.pozo.2002@gmail.com>
2023-05-11 12:52:11 +0200

    Proyecto final

--- src/Proyecto/Problema.java
+++ src/Proyecto/Problema.java
@@ -1,17 +1,58 @@
 package Proyecto;
 
 import java.io.*;
-import java.util.regex.Pattern;
+import java.security.PrivateKey;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Problema {
 
-    private static boolean validacion(String texto){
+
+    /*private static boolean validacion(String texto){
         String exReg = "[a-zA-Z]";
         Pattern pt = Pattern.compile(exReg);
+        Matcher match = pt.matcher(texto);
+    }*/
 
+    private static int burbuja(int[] A) {
+        int i, j, aux;
+        for (i = 0; i < A.length - 1; i++) {
+            for (j = 0; j < A.length - i - 1; j++) {
+                if (A[j + 1] < A[j]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+        return i;
+    }
+
+    private static Integer sacarEnter( String texto, String clave){
+        int[] arrayInteger = new int[26];
+        for (int i = 0; i < 26; i++) {
+            arrayInteger[i] = i;
+        }
+        String[] arrayString = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        Map<Integer, String> cript = new HashMap<>();
+        for (int i = 0; i < arrayString.length; i++) {
+            cript.put(arrayInteger[i], arrayString[i].toUpperCase(Locale.ROOT));
+        }
+
+        int[] burbujaTexto = new int[texto.length()];
+        int[] burbujaValor = new int[texto.length()];
+        for (int i = 0; i < texto.length(); i++) {
+            texto.toUpperCase(Locale.ROOT);
+            burbujaTexto[i] = texto.charAt(i);
+            burbujaValor[i] = texto.charAt(i);
+        }
+        return burbuja(burbujaTexto) + burbuja(burbujaValor);
     }
 
     public static void main(String[] args) {
+
 
         BufferedReader br = null;
         BufferedWriter bw = null;
@@ -22,17 +63,23 @@ public class Problema {
 
             String linea = null;
 
-            /* Lectura y validación  de clave */
+            Scanner sc = new Scanner(System.in);
+            String clave = "";
+            do {
+                System.out.println("Clave: ");
+                clave = sc.nextLine();
+            } while(clave.length() > 12);
+
 
             while ((linea = br.readLine()) != null) {
                 StringBuilder sb = new StringBuilder(linea.length());
+                Map<Integer, String> cript = new HashMap<>();
+                String r = br.readLine();
+                sacarEnter( r, clave);
 
 
-                /* Aquí vendría la lógica del programa */
 
-
-
-                bw.write(sb.toString()); /* Escribe la cadena de caracteres en el fichero*/
+                bw.write(sacarEnter( r, clave)); /* Escribe la cadena de caracteres en el fichero*/
                 bw.newLine(); /* escribe nueva línea en el fichero */
 
             }
